1e8477906f8e753b02cfa2e526e798535b206331
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-09-08 18:00:48 -0300

    creating and organizing the project

---
+++ app/src/main/java/com/alanmxll/nybooks/presentation/books/BooksActivity.kt
@@ -0,0 +1,12 @@
+package com.alanmxll.nybooks.presentation.books
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alanmxll.nybooks.R
+
+class BooksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+    }
+}
