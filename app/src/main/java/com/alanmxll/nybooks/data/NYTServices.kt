044dcf6a1e148a8e29db056a406a4e626079f0ed
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-09-12 11:49:18 -0300

    finalizing the network layer, mapping the API

--- app/src/main/java/com/alanmxll/nybooks/data/NYTServices.kt
+++ app/src/main/java/com/alanmxll/nybooks/data/NYTServices.kt
@@ -1,10 +1,15 @@
 package com.alanmxll.nybooks.data
 
+import com.alanmxll.nybooks.data.response.BookBodyResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface NYTServices {
 
     @GET("list.json")
-    fun listRepos(): Call<List<>>
+    fun getBooks(
+        @Query("api-key") apiKey: String = "YOUR API KEY",
+        @Query("list") list: String = "hardcover-fiction"
+    ): Call<BookBodyResponse>
 }
